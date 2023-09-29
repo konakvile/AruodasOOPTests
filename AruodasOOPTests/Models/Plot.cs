9649735b3c2dca023d8f2ea7c3de352198fc9536
konakvile <kondrotaite.a@gmail.com>
2023-09-29 16:42:34 +0100

    Changes to refractor

--- AruodasOOPTests/Models/Plot.cs
+++ AruodasOOPTests/Models/Plot.cs
@@ -11,80 +11,49 @@ namespace AruodasOOPTests.Models
 {
     internal class Plot : RealEstate
     {
+        
+        public string StreetNo { get; set; }
+        public bool ShowStreetNo { get; set; }
        
-
         /* Purpose Checkbox values
          * "1" Residential land, "2" Manufacturing land, "3" Agricultural, "4" Collective garden, 
          * "5" Forestrial, "6" Factory, "7" Storage, "8" Commercial, "9" Recreational, "10" Other */
         public string[] PurposeCB { get; set; }
 
-        // additional checkboxes can be added later on
-        public string Description { get; set; }
-        public string UpPhotos { get; set; } 
-        public string YoutubeLink { get; set; }
-        public string Tour3DLink { get; set; }
-        public string LotPrice { get; set; }
-        public string PhoNo { get; set; }
-        public string Email { get; set; }
-        public bool DoNotShowEmail { get; set; }
-        public bool TurnOffChat { get; set; }
-        public bool AcceptTandC { get; set; }
-
-
+        
         public Plot () : base() 
         { 
         }
 
         // testams tik del adreso dalies pildymo
-        public Plot(string region, string settlement, string microdistrict, string street, string streetNo, bool showStreetNo) : base()
+        public Plot(string region, string settlement, string microdistrict, string street, 
+            string streetNo, bool showStreetNo) : base(region, settlement, microdistrict, street)
         {
-            Region = region;
-            Settlement = settlement;
-            Microdisctrict = microdistrict;
-            Street = street;
             StreetNo = streetNo;
             ShowStreetNo = showStreetNo;
         }
 
         // minimalus objekto uzpildymas (raudonuojantys laukai, e-mail neitrauktas nes buna pre-filled)
-        public Plot (string region, string settlement, string area, string lotPrice, string phoNo, bool acceptTandC, string[] checkBoxes) : base()
+        public Plot (string region, string settlement, string area, string lotPrice, string phoNo, bool acceptTandC, string[] checkBoxes)
+            : base(region, settlement, lotPrice, phoNo, acceptTandC)
         {
-            Region = region;
-            Settlement = settlement;
             Area = area;
-            LotPrice = lotPrice;
-            PhoNo = phoNo;
-            AcceptTandC = acceptTandC;
             PurposeCB = checkBoxes;
         }
         // maksimalus objekto uzpildymas (butini ir nebutini laukai)
         public Plot(string region, string settlement, string microdistrict, string street, string streetNo, 
             bool showStreetNo, string uniqItemNo, bool showuniqItemNo, string area, string[] purposeCB, 
             string description, string upPhoto, string youtubeLink, string tour3DLink, string lotPrice, 
-            string phoNo, string email, bool doNotShowEmail, bool turnOffChat, bool acceptTandC) : base()
+            string phoNo, string email, bool doNotShowEmail, bool turnOffChat, bool acceptTandC) : base(region, 
+                settlement, microdistrict, street, uniqItemNo, showuniqItemNo, area, description, upPhoto, 
+                youtubeLink, tour3DLink, lotPrice, phoNo, email, doNotShowEmail, turnOffChat, acceptTandC)
         {
-            Region = region;
-            Settlement = settlement;
-            Microdisctrict = microdistrict;
-            Street = street;
             StreetNo = streetNo;
             ShowStreetNo = showStreetNo;
-            UniqItemNo = uniqItemNo;
-            ShowUniqItemNo = showuniqItemNo;
-            Area = area;
-            PurposeCB = purposeCB;
-            Description = description;
-            UpPhotos = upPhoto;
-            YoutubeLink = youtubeLink;
-            Tour3DLink = tour3DLink;
-            LotPrice = lotPrice;
-            PhoNo = phoNo;
-            Email = email;
-            DoNotShowEmail = doNotShowEmail;
-            TurnOffChat = turnOffChat;
-            AcceptTandC = acceptTandC;
+            PurposeCB = purposeCB;   
         }
 
+        /* Leaving the code for learning purposes, cannot transfer to RealEstate class
         public void FillInListingMin() // first test, will not select district
         {
             Driver.Navigate().GoToUrl("https://www.aruodas.lt/ideti-skelbima/?obj=11&offer_type=1");
@@ -108,7 +77,7 @@ namespace AruodasOOPTests.Models
             if (this.AcceptTandC){
                 Driver.FindElement(By.XPath("//*[@id=\"newObjectForm\"]/ul/li[38]/span[1]/div/div/label/span")).Click();
             } 
-        }
+        } */
 
         public void FillInListingMax()
         {
@@ -134,123 +103,10 @@ namespace AruodasOOPTests.Models
 
         public void SetLocation()
         {
-            SetRegion();
-            SetSettlement();
-            try { SetMicrodistrict(); }
-            catch { Console.WriteLine("Microdistrict field is hidden"); }
-            SetStreet();
+           base.SetLocation();
             SetStreetNo();
             SetShowStreetNo();
-
         } 
-
-        public void SetRegion()
-        {
-            Driver.FindElement(By.XPath("//*[@id=\"newObjectForm\"]/ul/li[3]/span[1]/input[2]")).Click();
-
-            int regionCount = Driver.FindElement(By.Id("regionDropdown")).FindElements(By.TagName("li")).Count;
-            Console.WriteLine("SUSKAICIUOTOS SAVIVALDYBES: " + regionCount);
-
-            Driver.FindElement(By.XPath("//*[@id=\"regionDropdown\"]/li[1]/input")).SendKeys(this.Region);
-
-            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
-
-            Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"regionDropdown\"]/li[" + (regionCount + 1) + "]"))).Click();
-            
-        }
-
-        public void SetSettlement()
-        {
-
-            Driver.FindElement(By.XPath("//*[@id=\"district\"]")).Click();
-           
-            IList<IWebElement> lis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[1].FindElements(By.TagName("li"));
-            Console.WriteLine("SUSKAICIUOTOS GYVENVIETES: " + lis.Count);
-
-            // Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
-            // Wait.Until(ExpectedConditions.ElementExists(By.Id("district").FindElement(By.ClassName("dropdown-input-values-address"))).FindElement(By.TagName("li[" + (settlementCount + 1) + "]"))).Click();
-            //*[@id="districts_461"]/li[89]
-
-            if (lis.Count > 14)
-            {
-
-                Driver.FindElement(By.Id("district")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Settlement);
-                Thread.Sleep(2000);
-                Driver.FindElement(By.Id("district")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
-            }
-            else 
-            {
-                for (int i = 0; i < lis.Count; i++)
-                {
-                    if (lis[i].Text.Contains(this.Settlement))
-                    {
-                        lis[i].Click();
-                        break;
-                    }
-                }
-            }
-        }
-
-        public void SetMicrodistrict()
-        {
-            Driver.FindElement(By.Id("quartalTitle")).Click();
-
-            IList<IWebElement> microLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[2].FindElements(By.TagName("li"));
-            Console.WriteLine("SUSKAICIUOTI MIKRORAJONAI: " + microLis.Count);
-
-            if (microLis.Count > 14)
-            {
-            
-                Driver.FindElement(By.Id("quartalField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Microdisctrict);
-                Thread.Sleep(2000);
-                Driver.FindElement(By.Id("quartalField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
-            }
-            else
-            {
-                for (int i = 0; i < microLis.Count; i++)
-                {
-                    if (microLis[i].Text.Contains(this.Microdisctrict))
-                    {
-                        microLis[i].Click();
-                        break;
-                    }
-                }
-            }
-
-        }
-
-        public void SetStreet()
-        {
-            Driver.FindElement(By.XPath("//*[@id=\"streetField\"]/span[1]")).Click();
-            IList<IWebElement> streetLis;
-            try
-            {
-                streetLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[3].FindElements(By.TagName("li"));
-            }catch(Exception e)
-            {
-                streetLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[2].FindElements(By.TagName("li"));
-            }
-            Console.WriteLine("SUSKAICIUOTOS GATVES: " + streetLis.Count);
-
-            if (streetLis.Count > 14)
-            {
-                Driver.FindElement(By.Id("streetField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Street);
-                Thread.Sleep(2000);
-                Driver.FindElement(By.Id("streetField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
-            }
-            else
-            {
-                for (int i = 0; i < streetLis.Count; i++)
-                {
-                    if (streetLis[i].Text.Contains(this.Street))
-                    {
-                        streetLis[i].Click();
-                        break;
-                    }
-                }
-            }
-
-        }
 
         public void SetStreetNo()
         {
--- AruodasOOPTests/Models/RealEstate.cs
+++ AruodasOOPTests/Models/RealEstate.cs
@@ -1,8 +1,11 @@
 ﻿using AruodasOOPTests.Helpers;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,22 +22,195 @@ namespace AruodasOOPTests.Models
         public string Settlement { get; set; }
         public string Microdisctrict { get; set; }
         public string Street { get; set; }
-        public string StreetNo { get; set; }
-        public bool ShowStreetNo { get; set; }
         public string UniqItemNo { get; set; }
         public bool ShowUniqItemNo { get; set; }
         public string Area { get; set; }
+        public string Description { get; set; }
+        public string UpPhotos { get; set; }
+        public string YoutubeLink { get; set; }
+        public string Tour3DLink { get; set; }
+        public string LotPrice { get; set; }
+        public string PhoNo { get; set; }
+        public string Email { get; set; }
+        public bool DoNotShowEmail { get; set; }
+        public bool TurnOffChat { get; set; }
+        public bool AcceptTandC { get; set; }
 
         public RealEstate() {
 
             this.Driver = DriverClass.Driver;
             this.Wait = DriverClass.Wait;
         }
-        public RealEstate()
+
+        public RealEstate(string region, string settlement, string microdistrict, string street, string uniqItemNo, bool showUniqItemNo, string area, string description, string upPhoto, string youtubeLink, string tour3DLink, string lotPrice,
+            string phoNo, string email, bool doNotShowEmail, bool turnOffChat, bool acceptTandC)
         {
 
             this.Driver = DriverClass.Driver;
             this.Wait = DriverClass.Wait;
+
+            Region = region;
+            Settlement = settlement;
+            Microdisctrict = microdistrict;
+            Street = street;
+            UniqItemNo = uniqItemNo;
+            ShowUniqItemNo = showUniqItemNo;
+            Area = area;
+            Description = description;
+            UpPhotos = upPhoto;
+            YoutubeLink = youtubeLink;
+            Tour3DLink = tour3DLink;
+            LotPrice = lotPrice;
+            PhoNo = phoNo;
+            Email = email;
+            DoNotShowEmail = doNotShowEmail;
+            TurnOffChat = turnOffChat;
+            AcceptTandC = acceptTandC;
+        }
+
+        // Tik minimalaus uzpildymo testams
+        public RealEstate (string region, string settlement, string lotPrice, string phoNo, bool acceptTandC)
+        {
+            this.Driver = DriverClass.Driver;
+            this.Wait = DriverClass.Wait;
+
+            Region = region;
+            Settlement = settlement;
+            LotPrice = lotPrice;
+            PhoNo = phoNo;
+            AcceptTandC = acceptTandC;
+
+        }
+
+        // Tik adreso pildymo testams
+        public RealEstate(string region, string settlement, string microdistrict, string street)
+        {
+            this.Driver = DriverClass.Driver;
+            this.Wait = DriverClass.Wait;
+
+            Region = region;
+            Settlement = settlement;
+            Microdisctrict = microdistrict;
+            Street = street;
+
+        }
+
+        public void SetRegion()
+        {
+            Driver.FindElement(By.XPath("//*[@id=\"newObjectForm\"]/ul/li[3]/span[1]/input[2]")).Click();
+
+            int regionCount = Driver.FindElement(By.Id("regionDropdown")).FindElements(By.TagName("li")).Count;
+            Console.WriteLine("SUSKAICIUOTOS SAVIVALDYBES: " + regionCount);
+
+            Driver.FindElement(By.XPath("//*[@id=\"regionDropdown\"]/li[1]/input")).SendKeys(this.Region);
+
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+
+            Wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[@id=\"regionDropdown\"]/li[" + (regionCount + 1) + "]"))).Click();
+
+        }
+
+        public void SetSettlement()
+        {
+
+            Driver.FindElement(By.XPath("//*[@id=\"district\"]")).Click();
+
+            IList<IWebElement> lis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[1].FindElements(By.TagName("li"));
+            Console.WriteLine("SUSKAICIUOTOS GYVENVIETES: " + lis.Count);
+
+            // Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+            // Wait.Until(ExpectedConditions.ElementExists(By.Id("district").FindElement(By.ClassName("dropdown-input-values-address"))).FindElement(By.TagName("li[" + (settlementCount + 1) + "]"))).Click();
+            //*[@id="districts_461"]/li[89]
+
+            if (lis.Count > 14)
+            {
+
+                Driver.FindElement(By.Id("district")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Settlement);
+                Thread.Sleep(2000);
+                Driver.FindElement(By.Id("district")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
+            }
+            else
+            {
+                for (int i = 0; i < lis.Count; i++)
+                {
+                    if (lis[i].Text.Contains(this.Settlement))
+                    {
+                        lis[i].Click();
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void SetMicrodistrict()
+        {
+            Driver.FindElement(By.Id("quartalTitle")).Click();
+
+            IList<IWebElement> microLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[2].FindElements(By.TagName("li"));
+            Console.WriteLine("SUSKAICIUOTI MIKRORAJONAI: " + microLis.Count);
+
+            if (microLis.Count > 14)
+            {
+
+                Driver.FindElement(By.Id("quartalField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Microdisctrict);
+                Thread.Sleep(2000);
+                Driver.FindElement(By.Id("quartalField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
+            }
+            else
+            {
+                for (int i = 0; i < microLis.Count; i++)
+                {
+                    if (microLis[i].Text.Contains(this.Microdisctrict))
+                    {
+                        microLis[i].Click();
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        public void SetStreet()
+        {
+            Driver.FindElement(By.XPath("//*[@id=\"streetField\"]/span[1]")).Click();
+            IList<IWebElement> streetLis;
+            try
+            {
+                streetLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[3].FindElements(By.TagName("li"));
+            }
+            catch (Exception e)
+            {
+                streetLis = Driver.FindElements(By.ClassName("dropdown-input-values-address"))[2].FindElements(By.TagName("li"));
+            }
+            Console.WriteLine("SUSKAICIUOTOS GATVES: " + streetLis.Count);
+
+            if (streetLis.Count > 14)
+            {
+                Driver.FindElement(By.Id("streetField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(this.Street);
+                Thread.Sleep(2000);
+                Driver.FindElement(By.Id("streetField")).FindElement(By.ClassName("dropdown-input-values-address")).FindElement(By.TagName("input")).SendKeys(Keys.Enter);
+            }
+            else
+            {
+                for (int i = 0; i < streetLis.Count; i++)
+                {
+                    if (streetLis[i].Text.Contains(this.Street))
+                    {
+                        streetLis[i].Click();
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        public virtual void SetLocation()
+        {
+            SetRegion();
+            SetSettlement();
+            try { SetMicrodistrict(); }
+            catch { Console.WriteLine("Microdistrict field is hidden"); }
+            SetStreet();
         }
 
     }
